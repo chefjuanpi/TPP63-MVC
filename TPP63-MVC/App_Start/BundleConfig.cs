f310b6d6534dc264ab8a5d80c92b595e0d5c8f51
pablo Aguilar Lliguin <chocolateria.arriba@gmail.com>
2014-06-25 16:17:35 -0400

    customize 1

--- TPP63-MVC/App_Start/BundleConfig.cs
+++ TPP63-MVC/App_Start/BundleConfig.cs
@@ -21,11 +21,13 @@ namespace TPP63_MVC
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/fotorama.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/fotorama.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
