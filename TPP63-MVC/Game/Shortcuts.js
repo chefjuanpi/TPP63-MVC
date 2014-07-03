6662b34724b676f413a51ab7c62039257b585584
industwetrust <industwetrust@live.ca>
2014-07-03 11:33:45 -0400

    intégration du jeu débuté

---
+++ TPP63-MVC/Game/Shortcuts.js
@@ -0,0 +1,4 @@
+function ById(id) {return document.getElementById(id)}
+function ByName(name) {return document.getElementsByName(name)}
+function ByClass(className) {return document.getElementsByClassName(className)}
+function Remove(element) {element.parentElement.removeChild(element);}
---
+++ TPP63-MVC/Game/game.js
@@ -0,0 +1,138 @@
+var G = { // G: Game
+  BLOCK_SIZE: 20,
+  CLUSTER_MIN_SIZE: 3,
+  
+  Difficulties: [
+    {Name: "Facile", PieceCount: 3},
+    {Name: "Moyen", PieceCount: 4},
+    {Name: "Difficile", PieceCount: 5},
+    {Name: "Extrème", PieceCount: 6}
+  ],
+  
+  Sizes: [
+    {Name: "Petit", Width: 20, Height: 20},
+    {Name: "Moyen", Width: 30, Height: 25},
+    {Name: "Grand", Width: 40, Height: 30},
+  ],
+
+  Init: function(size, difficulty, container) {
+    G.Score = 0;
+  
+    G.Width = size.Width;
+    G.Height = size.Height;
+    
+    G.Difficulty = difficulty;
+    
+    G.Container = container;
+    
+    Block.SetBlockSize(G.BLOCK_SIZE);
+    G.FillGrid();
+    
+    container.onclick = function(event) {G.OnClick(event)};
+  },
+  
+  FillGrid: function() {
+    G.Grid = new Array();
+    var colorIndex;
+  
+    for (var x = 0; x < G.Width; x++) {
+      G.Grid[x] = [];
+      
+      for (var y = 0; y < G.Height; y++) {
+          //console.log("X: " + x + " ;Y: " + y);
+        colorIndex = Math.floor(Math.random() * G.Difficulty.PieceCount);
+        
+        G.Grid[x][y] = new Block.Block(x, y, Block.Colors[colorIndex]);
+        
+        G.Grid[x][y].Draw(G.Container);
+        //console.log("X: " + G.Grid[x][y].X + " ;Y: " + G.Grid[x][y].Y);
+      }
+    }
+  },
+  
+  GetSameColorCluster: function(x, y, colorIndex, cluster) {
+    var block = G.Grid[x][y];
+    
+    if (block === undefined || block.Visited || block.Color.Index !== colorIndex)
+      return cluster;
+      
+    block.Visited = true;
+  
+    cluster.push(block);
+    
+    if (x < G.Width-1)  cluster = G.GetSameColorCluster(x+1, y, colorIndex, cluster);
+    if (x > 0)          cluster = G.GetSameColorCluster(x-1, y, colorIndex, cluster);
+    if (y < G.Height-1) cluster = G.GetSameColorCluster(x, y+1, colorIndex, cluster);
+    if (y > 0)          cluster = G.GetSameColorCluster(x, y-1, colorIndex, cluster);
+    
+    return cluster;
+  },
+  
+  UpdateScore: function(clusterSize) {
+    G.Score += clusterSize*clusterSize * 10;
+  },
+  
+  DestroyBlocks: function(blocks) {
+    blocks.forEach(function(b) {
+      Remove(b.Image);
+      G.Grid[b.X][b.Y] = undefined;
+    });
+  },
+  
+  MakeBlocksFall: function() {
+    var dropSize;
+    
+    for (var x = 0; x < G.Width; x++) {
+      dropSize = 0;
+      
+      for (var y = G.Height-1; y >= 0; y--) {
+        if (G.Grid[x][y] === undefined)
+          dropSize++;
+        else if (dropSize > 0) {
+          G.Grid[x][y].Move(0, dropSize);
+          G.Grid[x][y + dropSize] = G.Grid[x][y];
+        }
+      }
+      
+      for (var y = dropSize - 1; y >= 0; y--) {
+        G.Grid[x][y] = undefined;
+      }
+    }
+  },
+  
+  CleanUpFloodFill: function() {
+    for (var x = 0; x < G.Width; x++) {
+      for (var y = 0; y < G.Height; y++) {
+        if (G.Grid[x][y] != undefined)
+          G.Grid[x][y].Visited = false;
+      }
+    }
+  },
+  
+  OnClick: function(e) {
+    var containerAbsPos = $(G.Container).offset();
+
+    var x = Math.floor((e.x - containerAbsPos.left) / G.BLOCK_SIZE);
+    var y = Math.floor((e.y - containerAbsPos.top) / G.BLOCK_SIZE);
+
+    console.log("X: " + x + "; Y: " + y);
+    
+    var block = G.Grid[x][y];
+    
+    if (block === undefined)
+      return;
+      
+    var cluster = G.GetSameColorCluster(x, y, block.Color.Index, new Array());
+    
+    if (cluster.length >= G.CLUSTER_MIN_SIZE) {
+      G.UpdateScore(cluster);
+      G.ScoreUpdated(G.Score);
+      G.DestroyBlocks(cluster);
+      G.MakeBlocksFall();
+    }
+    
+    G.CleanUpFloodFill();
+  },
+  
+  ScoreUpdated: function(newScore) {}
+};
