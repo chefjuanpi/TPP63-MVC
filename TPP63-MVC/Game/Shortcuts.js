5341d6c28e0681ccc499f3b7b6fec86c08070c81
industwetrust <industwetrust@live.ca>
2014-07-03 13:18:55 -0400

    menu de jeu débuté

--- TPP63-MVC/Game/Shortcuts.js
+++ TPP63-MVC/Game/Shortcuts.js
@@ -1,4 +1,6 @@
 function ById(id) {return document.getElementById(id)}
 function ByName(name) {return document.getElementsByName(name)}
-function ByClass(className) {return document.getElementsByClassName(className)}
-function Remove(element) {element.parentElement.removeChild(element);}
+function ByClass(className) { return document.getElementsByClassName(className) }
+
+function Remove(element) { element.parentElement.removeChild(element); }
+function TryRemove(element) { if (document.contains(element)) Remove(element); }
--- TPP63-MVC/Game/block.js
+++ TPP63-MVC/Game/block.js
@@ -1,43 +1,43 @@
 var Block = {
-  Colors: [
-    {Index: 0, Name: "Red"},
-    {Index: 1, Name: "Green"},
-    {Index: 2, Name: "Blue"},
-    {Index: 3, Name: "Cyan"},
-    {Index: 4, Name: "Orange"},
-    {Index: 5, Name: "Purple"},
-    {Index: 6, Name: "Yellow"},
-  ],
-  
-  SetBlockSize: function(size) {
-    Block.Size = size;
-  },
+    Colors: [
+      { Index: 0, Name: "Red" },
+      { Index: 1, Name: "Green" },
+      { Index: 2, Name: "Blue" },
+      { Index: 3, Name: "Cyan" },
+      { Index: 4, Name: "Orange" },
+      { Index: 5, Name: "Purple" },
+      { Index: 6, Name: "Yellow" },
+    ],
 
-  Block: function(posX, posY, color) {
-    var img;
-    img = document.createElement('img');
-    img.className = 'gameBlock';
-    img.src = 'Images/Game/' + color.Name + ' Block.png';
-    
-    this.X = posX;
-    this.Y = posY;
-    
-    this.Color = color;
-    this.Image = img;
-    this.Visited = false; // Sert à vérifier si le block a été visité par un flood fill. 
-  }
+    SetBlockSize: function (size) {
+        Block.Size = size;
+    },
+
+    Block: function (posX, posY, color) {
+        var img;
+        img = document.createElement('img');
+        img.className = 'gameBlock';
+        img.src = 'Images/Game/' + color.Name + ' Block.png';
+
+        this.X = posX;
+        this.Y = posY;
+
+        this.Color = color;
+        this.Image = img;
+        this.Visited = false; // Sert à vérifier si le block a été visité par un flood fill. 
+    }
 };
 
-Block.Block.prototype.Draw = function(container) {
-  this.Image.style.left = this.X * Block.Size + "px";
-  this.Image.style.top = this.Y * Block.Size + "px";
-  container.appendChild(this.Image);
+Block.Block.prototype.Draw = function (container) {
+    this.Image.style.left = this.X * Block.Size + "px";
+    this.Image.style.top = this.Y * Block.Size + "px";
+    container.appendChild(this.Image);
 };
 
-Block.Block.prototype.Move = function(x, y) {
-  this.X += x;
-  this.Y += y;
+Block.Block.prototype.Move = function (x, y) {
+    this.X += x;
+    this.Y += y;
 
-  this.Image.style.left = this.X * Block.Size + "px";
-  this.Image.style.top = this.Y * Block.Size + "px";
+    this.Image.style.left = this.X * Block.Size + "px";
+    this.Image.style.top = this.Y * Block.Size + "px";
 };
